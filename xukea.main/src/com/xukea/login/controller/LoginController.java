6125d0cee75ecc562c36a0a5b237d1159b7b920b
HongKing <test@k-rong.com>
2013-04-12 14:58:53 +0800

    登录退出处理

--- xukea.main/src/com/xukea/login/controller/LoginController.java
+++ xukea.main/src/com/xukea/login/controller/LoginController.java
@@ -7,16 +7,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.xukea.common.UserBasicInfo;
+import com.xukea.common.filter.security.SecurityLoginFilter;
+import com.xukea.common.filter.security.SecuritySessionFilter;
+import com.xukea.common.util.WebUtil;
 import com.xukea.framework.base.BaseRestSpringController;
+import com.xukea.framework.util.ContextUtil;
+import com.xukea.framework.util.crypto.MD5;
 import com.xukea.main.role.model.Menu;
+import com.xukea.main.role.model.Role;
 import com.xukea.main.role.service.MenuService;
+import com.xukea.main.role.service.RoleService;
 import com.xukea.main.user.model.User;
+import com.xukea.main.user.service.UserService;
 
 
 /**
@@ -29,7 +41,13 @@ import com.xukea.main.user.model.User;
 public class LoginController extends BaseRestSpringController<User, Long>{
 
 	@Resource
-	private MenuService menuService; 
+	private MenuService menuService;
+	
+	@Resource
+	private RoleService roleService; 
+
+	@Resource
+	private UserService userService;
 	
 	/**
 	 * 首页
@@ -43,38 +61,75 @@ public class LoginController extends BaseRestSpringController<User, Long>{
 
 	@RequestMapping(value="/login")
 	public String getPageLogin(HttpServletRequest request, HttpServletResponse response) {
-		return "/login/login";
+		return "redirect:/login";
 	}
 
 	/**
-	 * 登录
+	 * 登录（如果采用spring security，则登陆将由{@link com.xukea.common.filter.security.LoginFilter}来验证）
 	 * @param request
 	 * @param response
 	 * @throws Exception
 	 */
 	@RequestMapping(value="/login", method=RequestMethod.POST)
 	public String login(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		UserBasicInfo userInfo = new UserBasicInfo();
-		
-		//TODO get user info
-		User user = new User();
-		user.setId(0);
-		user.setUserName("system");
-		user.setRealName("系统用户");
-		user.setEmail("xuhz@broaderonline.com");
+    	// 获取request参数
+    	String username = WebUtil.getValueString(request, "username", "");
+    	String password = WebUtil.getValueString(request, "password", "");
+    	
+    	// TODO 验证码校验
+
+    	// 基本校验request参数
+    	if("".equals(username)){
+    		request.setAttribute("ERROR_MSG", "用户名不能为空");
+    		return "redirect:/login";
+    	}
+    	if("".equals(password)){
+    		request.setAttribute("ERROR_MSG", "密码不能为空");
+    		return "redirect:/login";
+    	}
+    	
+    	// 查询用户信息
+    	User user = userService.getUserByUserName(username); 
+    	if(user==null){
+    		request.setAttribute("ERROR_MSG", "用户不存在");
+    		return "redirect:/login";
+    	}
+    	
+    	// 密码校验
+    	String newpswd = MD5.encrypt(password);
+    	newpswd = MD5.encrypt(newpswd+username);
+		if(!newpswd.equalsIgnoreCase(user.getPassword())){
+    		request.setAttribute("ERROR_MSG", "密码错误");
+    		return "redirect:/login";
+		}
 		
 		//set user to UserBasicInfo
+		UserBasicInfo userInfo = new UserBasicInfo();
 		userInfo.setUserId(user.getId());
 		userInfo.setUserName(user.getUserName());
 		userInfo.setRealName(user.getRealName());
 		userInfo.setEmail(user.getEmail());
 		
-		//get menu
+		// get user's menu
 		List<Menu> menus = menuService.getSubMenuByUID(user.getId(), "");
-		
 		userInfo.setAttribute("MENU", menus);
-		UserBasicInfo.saveToSession(request, userInfo);
 		
+		// get user's role
+		List<Role> roles = roleService.getRoleByUserId(user.getId());
+		userInfo.setAttribute("ROLES", roles);
+		
+		/****************** 整合用户数据到Sping security ***********************/
+		// 生成用户token
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user.getUserName(), user.getPassword());
+		SecurityLoginFilter loginFilter = ContextUtil.getBean(SecurityLoginFilter.class);
+        loginFilter.setDetails(request, authToken);
+        // 生成Spring Security的UserDetail对象中的权限等信息
+        Authentication authentication = loginFilter.getAuthenticationManager().authenticate(authToken);
+        // 更新Spring Security中的用户权限信息
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+		// save to session
+		UserBasicInfo.saveToSession(request, userInfo);
 		return "redirect:/index";
 	}
 
@@ -87,9 +142,15 @@ public class LoginController extends BaseRestSpringController<User, Long>{
 	 */
 	@RequestMapping(value="/logout")
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		/****************** 删除Sping security中对应的UserDetails ***********************/
+		SecurityContextHolder.clearContext();
+
+		/****************** 删除SESSION中的数据 ***********************/
 		HttpSession session = request.getSession();
 		//删除在线状态等session销毁处理迁移至SessionListener中处理，由session.invalidate()触发执行
 		session.invalidate();
+		
 		return new ModelAndView("redirect:/login");
 	}
 }
