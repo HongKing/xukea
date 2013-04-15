91a3aab9c2be81b79e1c97226abcf9edcd3920aa
HongKing <test@k-rong.com>
2013-04-15 16:55:58 +0800

    用户管理初始化数据

--- xukea.main/doc/db/v1.0/mysql/main_initdata.sql
+++ xukea.main/doc/db/v1.0/mysql/main_initdata.sql
@@ -1,7 +1,6 @@
 --初始设置
 INSERT INTO `usr_user` (`ID`, `USER_NAME`, `REAL_NAME`, `PASSWORD`, `SEX`, `MOBILE`, `EMAIL`, `TELEPHONE`, `ADDRESS`, `POSTCODE`, `STATUS`, `ADD_DATE`, `EDIT_DATE`) VALUES
 (0, 'system', '系统', 'D5F416F772B0B6073845712F8D10DA1D', '0', '', 'admin@xukea.com', '', '', '', '', now(), NULL);
-
 INSERT INTO `plugin_info` (`CODE`, `NAME`, `VERSION`, `STATUS`, `LEVEL`, `SETUP_DATE`) VALUES ('000', '系统默认', '1.0.0', '1', '0', now());
 INSERT INTO `usr_role` (`CODE`, `NAME`, `SHORT_WORD`, `CAN_EDIT`, `CAN_DELET`, `ORDER_IDX`) VALUES ('000', '通用权限',   'PUBLIC_ROLE', 0, 0, 1);
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`, `ORDER_IDX`) VALUES ('000', '首页',     'MAIN_INDEX', '/index', '000', 1);
@@ -15,14 +14,13 @@ INSERT INTO `usr_role` (`CODE`, `NAME`, `SHORT_WORD`, `CAN_EDIT`, `CAN_DELET`, `
 --用户角色对应关系
 INSERT INTO `usr_user_role` (`USER_ID`, `ROLE_CODE`) VALUES (0, '001');
 
---插件
+--插件（系统基础框架）
 INSERT INTO `plugin_info` (`CODE`, `NAME`, `VERSION`, `STATUS`, `LEVEL`, `SETUP_DATE`) VALUES ('001', '系统基础框架', '1.0.0', '1', '0', now());
 
---菜单资源
+--系统管理菜单资源
 -- 首级菜单
-
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`, `ORDER_IDX`) VALUES ('001', '系统管理', 'MAIN_SYS',    '/main',  '001', 999);
-
+-- 每层菜单如果有子菜单、快捷菜单等时，都需要定义以下三个目录，并将内容放置到对应的目录下
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001', '系统管理子菜单',   'MAIN_SYS_SUB', '', '001');
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001002', '系统管理快捷菜单', 'MAIN_SYS_QUK', '', '001');
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001003', '系统管理相关资源', 'MAIN_SYS_RSC', '', '001');
@@ -47,7 +45,6 @@ INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) 
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001003001', '系统配置子菜单',   'MAIN_SYS_SETING_SUB', '', '001');
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001003002', '系统配置快捷菜单', 'MAIN_SYS_SETING_QUK', '', '001');
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001003003', '系统配置相关资源', 'MAIN_SYS_SETING_RSC', '', '001');
-
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001003001001', '基本设置',   'MAIN_SYS_SETING_SUB_BASI', '/main/setting/basic', '001');
 INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001003001002', '邮件设置',   'MAIN_SYS_SETING_SUB_MAIL', '/main/setting/email', '001');
 
@@ -55,7 +52,7 @@ INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) 
 INSERT INTO `usr_mdl_group` (`CODE`, `NAME`, `SHORT_WORD`, `PLUGIN_CODE`) VALUES ('001', '系统用户管理', 'MAIN_SYS_USER', '001');
 INSERT INTO `usr_mdl_group` (`CODE`, `NAME`, `SHORT_WORD`, `PLUGIN_CODE`) VALUES ('002', '系统插件管理', 'MAIN_SYS_PLUGIN', '001');
 
---模块资源对应关系
+--模块分组与模块资源对应关系
 INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('001', '001');
 INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('001', '001001001');
 INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('001', '001001002');
@@ -76,7 +73,19 @@ INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('002', '0010
 INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('002', '001001003001001');
 INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('002', '001001003001002');
 
---模块角色对应关系
+--模块组与角色对应关系
 INSERT INTO `usr_mdl_group_role` (`ROLE_CODE`, `MDLG_CODE`) VALUES ('001', '001');
 INSERT INTO `usr_mdl_group_role` (`ROLE_CODE`, `MDLG_CODE`) VALUES ('001', '002');
 
+
+
+-- 用户管理子菜单设置
+INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001001001', '用户管理子菜单',   'MAIN_SYS_USER_SUB', '', '001');
+INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001001002', '用户管理快捷菜单', 'MAIN_SYS_USER_QUK', '', '001');
+INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001001003', '用户管理相关资源', 'MAIN_SYS_USER_RSC', '', '001');
+INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001001003001', '用户列表', 'MAIN_SYS_USER_RSC_LIST', '/main/user/list/{page}', '001');
+INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001001003002', '用户新增', 'MAIN_SYS_USER_RSC_NEW',  '/main/user/new',         '001');
+INSERT INTO `usr_mdl_menu` (`CODE`, `NAME`, `SHORT_WORD`, `URL`, `PLUGIN_CODE`) VALUES ('001001001003003', '用户编辑', 'MAIN_SYS_USER_RSC_EDIT', '/main/user/edit/{uid}',  '001');
+INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('001', '001001001003001');
+INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('001', '001001001003002');
+INSERT INTO `usr_mdl_group_menu` (`MDLG_CODE`, `MENU_CODE`) VALUES ('001', '001001001003003');
